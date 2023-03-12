27a1af927ba98d523d7d6032da9ea85f636779ea
blissful <blissful@sunsetglow.net>
2023-03-12 04:08:59 -0400

    link active state

--- frontend/foundation/ui/Link/index.css.ts
+++ frontend/foundation/ui/Link/index.css.ts
@@ -8,14 +8,17 @@ export const sLink = recipe({
         color: t.color.content.brand.default,
         textDecoration: "underline",
         textDecorationColor: t.color.content.brand.default,
-        ":hover": {
-          color: t.color.content.brand.hover,
-        },
+        ":hover": { color: t.color.content.brand.hover },
+        ":active": { color: t.color.content.brand.hover },
       },
       secondary: {
         textDecoration: "underline",
         textDecorationColor: t.color.content.neutral.weak,
         ":hover": {
+          color: t.color.content.neutral.strong,
+          textDecorationColor: t.color.content.neutral.strong,
+        },
+        ":active": {
           color: t.color.content.neutral.strong,
           textDecorationColor: t.color.content.neutral.strong,
         },
