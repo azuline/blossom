c75ed68d25669257b147a7f4b1e25bdc586d6984
blissful <blissful@sunsetglow.net>
2023-03-13 01:39:00 -0400

    font face definitions

--- frontend/foundation/theme/themes/type.css.ts
+++ frontend/foundation/theme/themes/type.css.ts
@@ -3,6 +3,7 @@ import { createGlobalTheme } from "@vanilla-extract/css";
 export const fontFaces = {
   display: "Cormorant Garamond",
   body: "Alegreya Sans",
+  code: "Source Code Pro",
 } as const;
 
 export const themeTypeVars = createGlobalTheme(":root", {
