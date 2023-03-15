b83bdfb1d089693267da20bf08af4002d719a1a7
blissful <blissful@sunsetglow.net>
2023-03-15 00:34:33 -0400

    remove icon from not found page

--- frontend/product/notfound/page/index.tsx
+++ frontend/product/notfound/page/index.tsx
@@ -1,4 +1,3 @@
-import { Icon } from "@foundation/icons/Icon";
 import { PageContent } from "@foundation/layout/PageContent";
 import { Button } from "@foundation/ui/Button";
 import { Flex } from "@foundation/ui/Flex";
@@ -7,10 +6,7 @@ import { Type } from "@foundation/ui/Type";
 const NotFoundPage: React.FC = () => (
   <PageContent center>
     <Flex sx={{ direction: "column", gap: "44" }}>
-      <Flex sx={{ align: "center", gap: "44" }}>
-        <Icon icon="logo" size="xxl" />
-        <Type variant="disp-xl">404 Page Not Found</Type>
-      </Flex>
+      <Type variant="disp-xl">404 Page Not Found</Type>
       <Button href="/">Return home</Button>
     </Flex>
   </PageContent>
