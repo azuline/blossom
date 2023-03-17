6a7b304404cfcbfd99b67ae261852a4fc62a9c1d
blissful <blissful@sunsetglow.net>
2023-03-16 22:44:41 -0400

    button cleanup

--- frontend/foundation/ui/Button/index.tsx
+++ frontend/foundation/ui/Button/index.tsx
@@ -18,14 +18,13 @@ export const Button: React.FC<ButtonProps> = props => {
     disabled: props.disabled,
     size: props.size,
     fullWidth: props.fullWidth,
-    // active: isPressed,
   });
 
   if ("as" in props && props.as === "a") {
     return (
       <Redirect {...props} className={clsx(sty, props.className)}>
         {/* Open a stacking context so the content is always on top of the hover overlay. */}
-        <Center sx={{ w: "full", h: "full", isolation: "isolate" }}>
+        <Center sx={{ isolation: "isolate" }}>
           {props.children}
         </Center>
       </Redirect>
@@ -35,7 +34,7 @@ export const Button: React.FC<ButtonProps> = props => {
   return (
     <Touchable {...(props as TouchableProps)} className={clsx(sty, props.className)}>
       {/* Open a stacking context so the content is always on top of the hover overlay. */}
-      <Center sx={{ w: "full", h: "full", isolation: "isolate" }}>
+      <Center sx={{ isolation: "isolate" }}>
         {props.children}
       </Center>
     </Touchable>
