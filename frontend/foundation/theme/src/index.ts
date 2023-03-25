ad6a76dce93bcb2252e14ffbfaf8be636be5c068
blissful <blissful@sunsetglow.net>
2023-03-25 17:53:34 -0400

    theme

---
+++ frontend/foundation/theme/src/index.ts
@@ -0,0 +1,7 @@
+// TODO: Can we make this its own package?
+import './styles/global.css';
+
+export * from './utils';
+export * from './styles';
+export * from './styles/sprinkles.css';
+export * from './provider';
