aace9d6a3680ed49f02f11378be0dcafd0e67e66
blissful <blissful@sunsetglow.net>
2023-03-26 03:06:50 -0400

    unimported

--- frontend/scripts/lintcheck.sh
+++ frontend/scripts/lintcheck.sh
@@ -5,6 +5,5 @@ exitcode=0
 pnpm eslint --ext .js,.cjs,.ts,.tsx --max-warnings=0 . || exitcode=$?
 dprint check --config "$BLOSSOM_ROOT/frontend/dprint.json" || exitcode=$?
 semgrep . --disable-version-check --strict --error --config "$BLOSSOM_ROOT/frontend/.semgrep" || exitcode=$?
-# TODO: Figure this out.
-# pnpm unimported || exitcode=$?
+pnpm unimported || exitcode=$?
 exit $exitcode
