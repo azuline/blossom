d3358ca6a1499381c5347974d098cc07ad12edc6
blissful <blissful@sunsetglow.net>
2023-09-01 22:54:44 -0400

    mm

--- frontend/configs/vitest/global.ts
+++ frontend/configs/vitest/global.ts
@@ -3,9 +3,9 @@
 
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // Learn more: https://github.com/testing-library/jest-dom
+import { afterEach } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
 
 // Resets all renders after each test.
 afterEach(() => cleanup());
