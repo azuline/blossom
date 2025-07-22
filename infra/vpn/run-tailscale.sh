0ee155f36bd815b0c6a45f96ee63188911a3fe9f
acid angel from asia <blissful@sunsetglow.net>
2025-07-21 17:59:28 -0700

    o

--- infra/vpn/run-tailscale.sh
+++ infra/vpn/run-tailscale.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 pid=$!
-until /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --advertise-routes="$ADVERTISE_ROUTES"; do sleep 0.1; done
+until /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --advertise-routes="$ADVERTISE_ROUTES" --hostname=render --reset; do sleep 0.1; done
 export ALL_PROXY=socks5://localhost:1055/
 echo "Tailscale is up at IP $(/app/tailscale ip)"
 wait ${pid}
