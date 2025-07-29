a61c107cece7b4b94d2b9aa5ae1051502dd070c7
acid angel from asia <blissful@sunsetglow.net>
2025-07-29 10:33:13 -0700

    playground

--- infra/gcp/gcp.tf
+++ infra/gcp/gcp.tf
@@ -7,7 +7,12 @@ terraform {
   }
 }
 
+# VARIABLES
+
 variable "project" {}
+variable "environment" {}
+
+# CLOUD
 
 provider "google" {
   project = var.project
@@ -15,21 +20,47 @@ provider "google" {
   zone    = "us-west1-a"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "production-net"
+# NETWORKS
+
+resource "google_compute_network" "primary" {
+  name = "${var.environment}-net"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "demo-instance"
-  machine_type = "e2-micro"
-  tags         = ["daemon"]
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
+# SERVICE ACCOUNTS
+
+resource "google_service_account" "god" {
+  account_id   = "sa-god"
+  display_name = "Emergency Break-Glass God Mode"
+}
+
+# DATABASES
+
+resource "google_sql_database_instance" "primary" {
+  name             = "main-instance"
+  region           = "us-west1"
+  database_version = "POSTGRES_16"
+  settings {
+    # TODO(when paying up, go to enterprise plus)
+    edition = "ENTERPRISE"
+    tier                        = "db-f1-micro"
+    deletion_protection_enabled = var.environment == "production"
+    disk_autoresize_limit       = 100
+    disk_size                   = 10
+    disk_type                   = "PD_SSD"
+    backup_configuration {
+      enabled                        = true
+      point_in_time_recovery_enabled = var.environment == "production"
+    }
+    ip_configuration {
+      private_network = "projects/${var.project}/global/networks/${google_compute_network.primary.name}"
+      enable_private_path_for_google_cloud_services = true
     }
   }
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {}
-  }
+  deletion_protection = true
+}
+
+resource "google_sql_database" "primary" {
+  name            = "main-database"
+  instance        = google_sql_database_instance.primary.name
+  deletion_policy = "ABANDON"
 }
